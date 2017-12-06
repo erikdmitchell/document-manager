95439842ec8329a425871dcc8273dc88c00ccc78
Erik <erikdmitchell@gmail.com>
2017-12-06 16:12:19 -0500

    bulk document import built out and tested

--- admin/pages/bulk-import.php
+++ admin/pages/bulk-import.php
@@ -1,22 +1,11 @@
 <h2>Bulk Import</h2>
 
 <form method="post" action="" method="post">
-	<?php wp_nonce_field('bulk_import', 'dm_admin_update'); ?>
+	<?php wp_nonce_field('bulk_import', 'dm_admin'); ?>
 
-	<h3 class="title"><?php _e('Options', 'document-manager'); ?></h3>
-
-	<table class="form-table options">
-		<tbody>
-			<tr>
-				<th scope="row"><label for="uploads_basefolder"><?php _e('Uploads Folder', 'document-manager'); ?></label></th>
-				<td>
-					<code><?php echo get_option('siteurl'); ?></code>
-					<input type="text" name="dm_settings[uploads_basefolder]" id="uploads_basefolder" class="regular-text code" value="<?php echo DocumentManager()->settings['uploads']['basefolder']; ?>" />
-					<p class="description"><?php _e('The folder documents should be uploaded to. Default is uploads folder. Pleaser enter the <strong>folder structure only</strong> i.e. /uploads', 'document-manager'); ?></p>
-				</td>
-			</tr>
-		</tbody>
-	</table>
-
-	<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Save Changes', 'document-manager'); ?>"></p>
+	<input type="url" name="dm_media_filename" id="dm-media-filename" class="regular-text code" value="">
+	<input id="dm-media-upload-button" type="button" class="button" value="<?php _e( 'Upload CSV File', 'document-manager' ); ?>" />
+	<input type="hidden" name="dm_file_attachment_id" id="dm-file-attachment-id" value="">
+	
+	<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Import', 'document-manager'); ?>"></p>
 </form>
