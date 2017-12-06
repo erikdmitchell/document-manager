55303aaddf9b59c8aeff12b8a9e9e4c277dc5c35
Erik <erikdmitchell@gmail.com>
2017-12-06 10:41:08 -0500

    added updater

--- document-manager.php
+++ document-manager.php
@@ -74,9 +74,11 @@ final class DocumentManager {
 
 	private function init_hooks() {
 		register_activation_hook(DM_PLUGIN_FILE, array('Document_Manager_Install', 'install'));
+		
+		add_action('admin_init', array($this, 'plugin_updater'));
+		add_action('init', array($this, 'get_settings'), 99);
 		add_action('init', array($this, 'init'), 0);
 		add_action('wp_enqueue_scripts', array($this, 'scripts_styles'));
-		add_action('init', array($this, 'get_settings'), 99);
 	}
 
 	public function init() {
@@ -90,6 +92,33 @@ final class DocumentManager {
 	public function scripts_styles() {
 		wp_enqueue_style('font-awesome', DM_URL.'css/font-awesome.min.css', '', '4.7.0');
 	}
+	
+	public function plugin_updater() {
+		if (!is_admin())
+			return false;
+	
+		if (!defined('WP_GITHUB_FORCE_UPDATE'))
+			define('WP_GITHUB_FORCE_UPDATE', true);
+			
+		$username='erikdmitchell';
+		$repo_name='pickle-document-manager';
+		$folder_name='pickle-document-manager';
+	    
+	    $config = array(
+	        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+	        'proper_folder_name' => $folder_name, // this is the name of the folder your plugin lives in
+	        'api_url' => 'https://api.github.com/repos/'.$username.'/'.$repo_name, // the github API url of your github repo
+	        'raw_url' => 'https://raw.github.com/'.$username.'/'.$repo_name.'/master', // the github raw url of your github repo
+	        'github_url' => 'https://github.com/'.$username.'/'.$repo_name, // the github url of your github repo
+	        'zip_url' => 'https://github.com/'.$username.'/'.$repo_name.'/zipball/master', // the zip url of the github repo
+	        'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+	        'requires' => '4.0', // which version of WordPress does your plugin require?
+	        'tested' => '4.9', // which version of WordPress is your plugin tested up to?
+	        'readme' => 'readme.txt', // which file to use as the readme for the version number
+	    );
+	   
+		new WP_GitHub_Updater($config);
+	}	
 
 }
 
