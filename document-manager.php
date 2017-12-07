c1e171938359e2614050915359f5ce271dd207bf
Erik <erikdmitchell@gmail.com>
2017-12-07 13:09:14 -0500

    primary class meets requirements

--- document-manager.php
+++ document-manager.php
@@ -20,16 +20,61 @@ if ( ! defined( 'DM_PLUGIN_FILE' ) ) {
     define( 'DM_PLUGIN_FILE', __FILE__ );
 }
 
+/**
+ * Final DocumentManager class.
+ *
+ * @final
+ */
 final class DocumentManager {
 
+    /**
+     * Document Manager version
+     *
+     * (default value: '1.0.0-alpha')
+     *
+     * @var string
+     * @access public
+     */
     public $version = '1.0.0-alpha';
 
+    /**
+     * Document Manager Admin class variable
+     *
+     * (default value: '')
+     *
+     * @var string
+     * @access public
+     */
     public $admin = '';
 
+    /**
+     * Document Manager settings
+     *
+     * (default value: '')
+     *
+     * @var string
+     * @access public
+     */
     public $settings = '';
 
+    /**
+     * Instance
+     *
+     * (default value: null)
+     *
+     * @var mixed
+     * @access protected
+     * @static
+     */
     protected static $_instance = null;
 
+    /**
+     * Instance setup.
+     *
+     * @access public
+     * @static
+     * @return instance
+     */
     public static function instance() {
         if ( is_null( self::$_instance ) ) {
             self::$_instance = new self();
@@ -38,12 +83,24 @@ final class DocumentManager {
         return self::$_instance;
     }
 
+    /**
+     * Construct class.
+     *
+     * @access public
+     * @return void
+     */
     public function __construct() {
         $this->define_constants();
         $this->includes();
         $this->init_hooks();
     }
 
+    /**
+     * Define constants.
+     *
+     * @access private
+     * @return void
+     */
     private function define_constants() {
         $this->define( 'DM_VERSION', $this->version );
         $this->define( 'DM_PATH', plugin_dir_path( __FILE__ ) );
@@ -51,12 +108,26 @@ final class DocumentManager {
 
     }
 
+    /**
+     * Custom define function.
+     *
+     * @access private
+     * @param mixed $name string.
+     * @param mixed $value string.
+     * @return void
+     */
     private function define( $name, $value ) {
         if ( ! defined( $name ) ) {
             define( $name, $value );
         }
     }
 
+    /**
+     * Include plugin files.
+     *
+     * @access public
+     * @return void
+     */
     public function includes() {
         include_once DM_PATH . 'class-dm-install.php';
         include_once DM_PATH . 'post-type.php';
@@ -72,6 +143,12 @@ final class DocumentManager {
         endif;
     }
 
+    /**
+     * Init hooks for plugin.
+     *
+     * @access private
+     * @return void
+     */
     private function init_hooks() {
         register_activation_hook( DM_PLUGIN_FILE, array( 'Document_Manager_Install', 'install' ) );
 
@@ -81,18 +158,42 @@ final class DocumentManager {
         add_action( 'wp_enqueue_scripts', array( $this, 'scripts_styles' ) );
     }
 
+    /**
+     * Init function.
+     *
+     * @access public
+     * @return void
+     */
     public function init() {
 
     }
 
+    /**
+     * Pull in settings.
+     *
+     * @access public
+     * @return void
+     */
     public function get_settings() {
         $this->settings = get_option( 'dm_settings', '' );
     }
 
+    /**
+     * Include front end scripts and styles.
+     *
+     * @access public
+     * @return void
+     */
     public function scripts_styles() {
         wp_enqueue_style( 'font-awesome', DM_URL . 'css/font-awesome.min.css', '', '4.7.0' );
     }
 
+    /**
+     * Setup plugin updater.
+     *
+     * @access public
+     * @return WP_GitHub_Updater
+     */
     public function plugin_updater() {
         if ( ! is_admin() ) {
             return false;
@@ -107,16 +208,16 @@ final class DocumentManager {
         $folder_name = 'pickle-document-manager';
 
         $config = array(
-            'slug'               => plugin_basename( __FILE__ ), // this is the slug of your plugin
-            'proper_folder_name' => $folder_name, // this is the name of the folder your plugin lives in
-            'api_url'            => 'https://api.github.com/repos/' . $username . '/' . $repo_name, // the github API url of your github repo
-            'raw_url'            => 'https://raw.github.com/' . $username . '/' . $repo_name . '/master', // the github raw url of your github repo
-            'github_url'         => 'https://github.com/' . $username . '/' . $repo_name, // the github url of your github repo
-            'zip_url'            => 'https://github.com/' . $username . '/' . $repo_name . '/zipball/master', // the zip url of the github repo
-            'sslverify'          => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+            'slug'               => plugin_basename( __FILE__ ), // this is the slug of your plugin.
+            'proper_folder_name' => $folder_name, // this is the name of the folder your plugin lives in.
+            'api_url'            => 'https://api.github.com/repos/' . $username . '/' . $repo_name, // the github API url of your github repo.
+            'raw_url'            => 'https://raw.github.com/' . $username . '/' . $repo_name . '/master', // the github raw url of your github repo.
+            'github_url'         => 'https://github.com/' . $username . '/' . $repo_name, // the github url of your github repo.
+            'zip_url'            => 'https://github.com/' . $username . '/' . $repo_name . '/zipball/master', // the zip url of the github repo.
+            'sslverify'          => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details.
             'requires'           => '4.0', // which version of WordPress does your plugin require?
             'tested'             => '4.9', // which version of WordPress is your plugin tested up to?
-            'readme'             => 'readme.txt', // which file to use as the readme for the version number
+            'readme'             => 'readme.txt', // which file to use as the readme for the version number.
         );
 
         new WP_GitHub_Updater( $config );
@@ -124,6 +225,12 @@ final class DocumentManager {
 
 }
 
+/**
+ * DocumentManager function.
+ *
+ * @access public
+ * @return instance
+ */
 function DocumentManager() {
     return DocumentManager::instance();
 }
