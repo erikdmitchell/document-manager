1102bbbe25fd2cbfd102355252130dc657540204
Erik <erikdmitchell@gmail.com>
2017-12-09 09:48:26 -0500

    added php codesniffer

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,15 @@ var uglify = require('gulp-uglify'); // uglify js plugin
 var pump = require('pump'); // gulp pump
 var cssnano = require('gulp-cssnano'); // minify css
 var sourcemaps = require('gulp-sourcemaps'); // use sourcemaps for css
-
-// var sass = require('gulp-sass'); // sass
+var sass = require('gulp-sass'); // sass
 var gutil = require('gulp-util'); // ultitly
 var livereload = require('gulp-livereload'); // auto reload
-// var autoprefixer = require('autoprefixer'); // adds browser prefixes
-// var cssdeclsort = require('css-declaration-sorter'); // orders our css within the class/id
-// var plumber = require('gulp-plumber'); // Prevent pipe breaking caused by errors from gulp plugins
-// var postcss = require('gulp-postcss'); // PostCSS is a tool for transforming styles with JS plugins
-// var rename = require('gulp-rename'); // rename files
+var autoprefixer = require('autoprefixer'); // adds browser prefixes
+var cssdeclsort = require('css-declaration-sorter'); // orders our css within the class/id
+var plumber = require('gulp-plumber'); // Prevent pipe breaking caused by errors from gulp plugins
+var postcss = require('gulp-postcss'); // PostCSS is a tool for transforming styles with JS plugins
+var rename = require('gulp-rename'); // rename files
+var phpcs = require('gulp-phpcs'); // Gulp plugin for running PHP Code Sniffer.
 	
 // Custom error function.
 var onError = function(err) {
@@ -121,153 +121,23 @@ gulp.task('mincss', function () {
         .pipe(gulp.dest(dirs.admin + '/css'));
 });
 			
-/*
+// Generate POT files.
+// Check textdomain errors.
+	
+// PHP Code Sniffer.
+var phpcsSrc = {
+    '**/*.php', // Include all files    
+    '!node_modules/**', // Exclude node_modules/
+	'!vendor/**' // Exclude vendor/    
+};
 
-		
-
-
-
-
-
-
-
-		// Watch changes for assets.
-		watch: {
-			css: {
-				files: ['<%= dirs.css %>/*.scss'],
-				tasks: ['sass', 'rtlcss', 'cssmin', 'concat']
-			},
-			js: {
-				files: [
-					'<%= dirs.js %>/admin/*js',
-					'<%= dirs.js %>/frontend/*js',
-					'!<%= dirs.js %>/admin/*.min.js',
-					'!<%= dirs.js %>/frontend/*.min.js'
-				],
-				tasks: ['jshint', 'uglify']
-			}
-		},
-
-		// Generate POT files.
-		// Check textdomain errors.
-		// Exec shell commands.
-		// Clean the directory.
-		/*
-		clean: {
-			apigen: {
-				src: [ 'wc-apidocs' ]
-			}
-		},
-        */
-		// PHP Code Sniffer.
-		phpcs: {
-			options: {
-				bin: 'vendor/bin/phpcs'
-			},
-			dist: {
-				options: {
-					standard: './phpcs.ruleset.xml'
-				},
-				src:  [
-					'**/*.php',                                                  // Include all files
-					'!apigen/**',                                                // Exclude apigen/
-					'!includes/api/legacy/**',                                   // Exclude legacy REST API
-					'!includes/gateways/simplify-commerce/includes/Simplify/**', // Exclude simplify commerce SDK
-					'!includes/libraries/**',                                    // Exclude libraries/
-					'!node_modules/**',                                          // Exclude node_modules/
-					'!tests/cli/**',                                             // Exclude tests/cli/
-					'!tmp/**',                                                   // Exclude tmp/
-					'!vendor/**'                                                 // Exclude vendor/
-				]
-			}
-		},
-
-		// Autoprefixer.
-		postcss: {
-			options: {
-				processors: [
-					require( 'autoprefixer' )({
-						browsers: [
-							'> 0.1%',
-							'ie 8',
-							'ie 9'
-						]
-					})
-				]
-			},
-			dist: {
-				src: [
-					'<%= dirs.css %>/*.css'
-				]
-			}
-		}
-	});
-
-
-
-
-	// Load NPM tasks to be used here
-	grunt.loadNpmTasks( 'grunt-sass' );
-	grunt.loadNpmTasks( 'grunt-shell' );
-	grunt.loadNpmTasks( 'grunt-phpcs' );
-	grunt.loadNpmTasks( 'grunt-rtlcss' );
-	grunt.loadNpmTasks( 'grunt-postcss' );
-	grunt.loadNpmTasks( 'grunt-stylelint' );
-	grunt.loadNpmTasks( 'grunt-wp-i18n' );
-	grunt.loadNpmTasks( 'grunt-checktextdomain' );
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
-	grunt.loadNpmTasks( 'grunt-contrib-concat' );
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-contrib-clean' );
-
-	// Register tasks
-	grunt.registerTask( 'default', [
-		'js',
-		'css',
-		'i18n'
-	]);
-
-	grunt.registerTask( 'js', [
-		'jshint',
-		'uglify:admin',
-		'uglify:frontend'
-	]);
-
-	grunt.registerTask( 'css', [
-		'sass',
-		'rtlcss',
-		'postcss',
-		'cssmin',
-		'concat'
-	]);
-
-	grunt.registerTask( 'docs', [
-		'clean:apigen',
-		'shell:apigen'
-	]);
-
-	// Only an alias to 'default' task.
-	grunt.registerTask( 'dev', [
-		'default'
-	]);
-
-	grunt.registerTask( 'i18n', [
-		'checktextdomain',
-		'makepot'
-	]);
-
-	grunt.registerTask( 'e2e-tests', [
-		'shell:e2e_tests'
-	]);
-
-	grunt.registerTask( 'e2e-tests-grep', [
-		'shell:e2e_tests_grep'
-	]);
-
-	grunt.registerTask( 'e2e-test', [
-		'shell:e2e_test'
-	]);
-};    
-*/
+gulp.task('phpcs', function () {
+    return gulp.src(['src/**/*.php', '!src/vendor/**/*.*'])
+        // Validate files using PHP Code Sniffer
+        .pipe(phpcs({
+            bin: 'vendor/bin/phpcs',
+            standard: './phpcs.ruleset.xml',
+            warningSeverity: 0
+        }))
+        .pipe(phpcs.reporter('log')); // Log all problems that was found
+});
